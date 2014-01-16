52724519dcceb80193e7966ffacfa52f1ea8d4c3
Shea Newton <sheanewt@gmail.com>
2014-01-16 13:18:40 -0800

    removed run.sh in favor of run-short.sh. Revised run-shootout in unisequential to reflect shootout times and to write to a file unicon-results.dat rather than results.dat

--- uni_sequential/run-shootout.sh
+++ uni_sequential/run-shootout.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 
+echo "" > unicon-results.dat
+echo binary-trees 20
+echo binary-trees 20 >> unicon-results.dat
+(time ./binary-trees 20 &>/dev/null) &>> unicon-results.dat
+echo "" >> unicon-results.dat
+echo fannkuch 12
+echo fannkuch 12 >> unicon-results.dat
+(time ./fannkuch 12 &>/dev/null) &>> unicon-results.dat
+echo "" >> unicon-results.dat
+echo fasta 25000000
+echo fasta 25000000 >> unicon-results.dat
+(time ./fasta 25000000 &>/dev/null) &>> unicon-results.dat
+echo "" >> unicon-results.dat
+echo k-nucleotide 25-mil.dat
+echo k-nucleotide 25-mil.dat >> unicon-results.dat
+(time ./k-nucleotide 25-mil.dat &>/dev/null) &>> unicon-results.dat
+echo "" >> unicon-results.dat
+echo mandelbrot 16000 
+echo mandelbrot 16000 >> unicon-results.dat
+(time ./mandelbrot 16000 &>/dev/null) &>> unicon-results.dat
+echo "" >> unicon-results.dat
+echo n-body 50000000
+echo n-body 50000000 >> unicon-results.dat
+(time ./n-body 50000000 &>/dev/null) &>> unicon-results.dat
+echo "" >> unicon-results.dat
+echo pidigits 10000
+echo pidigits 10000 >> unicon-results.dat
+(time ./pidigits 10000 &>/dev/null) &>> unicon-results.dat
+echo "" >> unicon-results.dat
+echo regex-dna 5-mil.dat
+echo regex-dna 5-mil.dat >> unicon-results.dat
+(time ./regex-dna 5-mil.dat &>/dev/null) &>> unicon-results.dat
+echo "" >> unicon-results.dat
+echo reverse-complement 25-mil.dat
+echo reverse-complement 25-mil.dat >> unicon-results.dat
+(time ./reverse-complement 25-mil.dat &>/dev/null) &>> unicon-results.dat
+echo "" >> unicon-results.dat
+echo spectral-norm 5500
+echo spectral-norm 5500 >> unicon-results.dat
+(time ./spectral-norm 5500 &>/dev/null) &>> unicon-results.dat
 
-echo "" > results.dat
-echo binary-trees 14
-echo binary-trees 14 >> results.dat
-(time ./binary-trees 14 &>/dev/null) &>> results.dat
-echo "" >> results.dat
-echo fannkuch 9
-echo fannkuch 9 >> results.dat
-(time ./fannkuch 9 &>/dev/null) &>> results.dat
-echo "" >> results.dat
-echo fasta 250000
-echo fasta 250000 >> results.dat
-(time ./fasta 250000 &>/dev/null) &>> results.dat
-echo "" >> results.dat
-echo k-nucleotide 150-thou.dat
-echo k-nucleotide 150-thou.dat >> results.dat
-(time ./k-nucleotide 150-thou.dat &>/dev/null) &>> results.dat
-echo "" >> results.dat
-echo mandelbrot 750 
-echo mandelbrot 750 >> results.dat
-(time ./mandelbrot 750 &>/dev/null) &>> results.dat
-echo "" >> results.dat
-echo n-body 10000
-echo n-body 10000 >> results.dat
-(time ./n-body 10000 &>/dev/null) &>> results.dat
-echo "" >> results.dat
-echo pidigits 7000
-echo pidigits 7000 >> results.dat
-(time ./pidigits 7000 &>/dev/null) &>> results.dat
-echo "" >> results.dat
-echo regex-dna 25-thou.dat
-echo regex-dna 25-thou.dat >> results.dat
-(time ./regex-dna 25-thou.dat &>/dev/null) &>> results.dat
-echo "" >> results.dat
-echo reverse-complement 25-thou.dat
-echo reverse-complement 25-thou.dat >> results.dat
-(time ./reverse-complement 25-thou.dat &>/dev/null) &>> results.dat
-echo "" >> results.dat
-echo spectral-norm 300
-echo spectral-norm 300 >> results.dat
-(time ./spectral-norm 300 &>/dev/null) &>> results.dat
-
-#(time (icon >/dev/null) >> results.dat)
+#(time (./ >/dev/null) >> unicon-results.dat)
